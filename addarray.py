2b937f911ed88bc31cb21e7057d1f1deefd626f8
vishal <vishalparmar3542@gmail.com>
2023-09-14 11:32:36 +0530

    all

---
+++ addarray.py
@@ -0,0 +1,9 @@
+import numpy as np
+in_num1 = 10
+in_num2 = 15
+ 
+print ("1st Input  number : ", in_num1)
+print ("2nd Input  number : ", in_num2)
+   
+out_num = np.add(in_num1, in_num2)
+print ("output number after addition  : ", out_num)
